1ca4fc068f220eaeaf9320b416f9beb387e5ef52
daocatt <daocatt@users.noreply.github.com>
2024-03-11 01:05:39 +0800

    fix loop date

--- source/script/index.js
+++ source/script/index.js
@@ -87,20 +87,21 @@
     let curDate = new Date();
     let newDesDate;
 
-      if(magLoop === 1){
-        newDesDate = new Date(curDate.getFullYear()+'-'+(desDate.getMonth() + 1)+'-'+desDate.getDate())
-        if(newDesDate.getTime() < curDate.getTime()){
-          newDesDate = new Date((curDate.getFullYear()+1)+'-'+(desDate.getMonth() + 1)+'-'+desDate.getDate())
-        }
-        dateDays = Math.floor((newDesDate.getTime() - curDate.getTime()) / (24*3600*1000))
-      } else {
-        dateDays = Math.floor((desDate.getTime() - curDate.getTime()) / (24*3600*1000))
+    if(magLoop === 1){
+      newDesDate = new Date(curDate.getFullYear()+'/'+(desDate.getMonth() + 1)+'/'+desDate.getDate());
+      
+      if(newDesDate.getTime() < curDate.getTime()){
+        newDesDate = new Date((curDate.getFullYear()+1)+'/'+(desDate.getMonth() + 1)+'/'+desDate.getDate());
       }
-      if(dateDays > 0){
-        $(elem).html('还有 <span class="day">'+dateDays+'</span> 天');
-      } else {
-        $(elem).html('距离<span>'+magText+'</span>已经 '+Math.abs(dateDays)+' 天');
-      }
+      dateDays = Math.floor((newDesDate.getTime() - curDate.getTime()) / (24*3600*1000));
+    } else {
+      dateDays = Math.floor((desDate.getTime() - curDate.getTime()) / (24*3600*1000));
+    }
+    if(dateDays > 0){
+      $(elem).html('还有 <span class="day">'+dateDays+'</span> 天');
+    } else {
+      $(elem).html('距离<span>'+magText+'</span>已经 '+Math.abs(dateDays)+' 天');
+    }
 
   });
 
